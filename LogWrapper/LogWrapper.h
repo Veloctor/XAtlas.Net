8270934e3d72d99ef8b1ee28aa82d9fc242b1886
矢速 <Velctor@outlook.com>
2024-03-12 11:11:41 +0800

    extern C for linux

--- LogWrapper/LogWrapper.h
+++ LogWrapper/LogWrapper.h
@@ -6,9 +6,9 @@
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(WINAPI_FAMILY)
 #define EXPORT extern "C" __declspec(dllexport)
 #elif defined(__MACH__) || defined(__ANDROID__) || defined(__linux__) || defined(LUMIN)
-#define EXPORT __attribute__ ((visibility ("default")))
+#define EXPORT extern "C" __attribute__ ((visibility ("default")))
 #else
-#define EXPORT
+#define EXPORT extern "C" 
 #endif
 
 typedef void (*StringFunc)(const char* formatted, int strLen);
